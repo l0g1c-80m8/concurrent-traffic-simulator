e7dec98e9cd10a2a277c7480cbae4db842d54894
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-09-27 23:25:25 -0700

    block vehicle entry until traffic light turns green

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -86,7 +86,9 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     lck.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
-    // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
+    // block the execution until the traffic light turns green.
+    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::red)
+        _trafficLight.waitForGreen();
 
     lck.unlock();
 }
@@ -140,13 +142,8 @@ void Intersection::simulate() // using threads + promises/futures + exceptions
 
 bool Intersection::trafficLightIsGreen()
 {
-   // please include this part once you have solved the final project tasks
-   /*
    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::green)
        return true;
    else
        return false;
-   */
-
-  return true; // makes traffic light permanently green
 } 
